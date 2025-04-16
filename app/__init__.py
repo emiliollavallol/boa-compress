6764e6c9c9a1f95ad7a276434f813833545ef0ab
emiliollavallol <emiliollavallol@gmail.com>
2025-04-16 17:09:10 -0300

    v2.0 commit sitemap y robots

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from app.compressor import compressor_bp
 from app.analyzer import analyzer_bp
 from app.contacto import contacto_bp
@@ -9,6 +9,15 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_key')
+
+    # Rutas para robots.txt y sitemap.xml
+    @app.route('/robots.txt')
+    def robots_txt():
+        return send_from_directory(app.root_path, 'robots.txt', mimetype='text/plain')
+
+    @app.route('/sitemap.xml')
+    def sitemap_xml():
+        return send_from_directory(app.root_path, 'sitemap.xml', mimetype='application/xml')
 
     # Registrar los Blueprints
     app.register_blueprint(compressor_bp, url_prefix='/comprimir')
